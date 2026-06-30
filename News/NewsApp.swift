57294bc3969ec9bd0e5225874d000bcc09e67d50
Parth Patel <4parth9@gmail.com>
2026-06-29 21:01:55 -0400

    Initial Commit

---
+++ News/NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsApp.swift
+//  News
+//
+//  Created by Parth Patel on 2026-06-29.
+//
+
+import SwiftUI
+
+@main
+struct NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
